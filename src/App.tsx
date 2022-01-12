f7d4cb2b85a60581ff3dc969893e30e3984d280d
splarsxd <splarsiz34@yahoo.com>
2022-01-12 23:54:46 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import './App.css'
+import {useState } from "react"
+import "./App.css"
+import Header from "./comp/Header"
+import Grid from "./comp/Grid"
 
 function App() {
 
+
+  const [ search,setter ]
+   = useState<string> ("")
+
+
   return (
-    <div className="App">
-      <p>hello</p>
+
+    <div className="component">
+
+
+      <Header searcher={ search } setter={ setter }/>
+       <Grid search={ search }/>
+
     </div>
   )
+
 }
 
+
 export default App
+
+
+
+
